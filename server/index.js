8d2288fcf92922b35745e48f5de0b8200edfdf0c
vikhyat singh <vikhyatsingh628@gmail.com>
2021-11-25 17:52:41 +0530

    make it better

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require('cors')
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const mongoose = require("mongoose");
+const connect = mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/product', require('./routes/product'));
+
+
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`)
+});
